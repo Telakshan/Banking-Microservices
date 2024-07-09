ca498cb09daaad702c6c0557eafff1cf4d184187
telakshan <telakshanb@gmail.com>
2024-07-09 18:35:34 -0400

    init commit

---
+++ accounts/accounts/src/main/java/com/quentin/tech/accounts/service/IAccountService.java
@@ -0,0 +1,10 @@
+package com.quentin.tech.accounts.service;
+
+import com.quentin.tech.accounts.dto.CustomerDto;
+
+public interface IAccountService {
+    void createAccount(CustomerDto customerDto);
+    CustomerDto fetchAccount(String mobileNumber);
+    boolean updateAccount(CustomerDto customerDto);
+    boolean deleteAccount(String mobileNumber);
+}
