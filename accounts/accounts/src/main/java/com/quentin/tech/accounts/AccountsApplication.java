fe34ce3728d1a30c588b54f25fb50f7a813a13b9
telakshan <telakshanb@gmail.com>
2024-07-20 12:19:06 -0400

    add open api definitions

--- accounts/accounts/src/main/java/com/quentin/tech/accounts/AccountsApplication.java
+++ accounts/accounts/src/main/java/com/quentin/tech/accounts/AccountsApplication.java
@@ -1,15 +1,30 @@
 package com.quentin.tech.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title="Accounts microservice REST API Documentation",
+				description = "QuentinTech accounts microservice REST API documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "John Doe"
+				),
+				license = @License(
+						name="MIT"
+				)
+		)
+)
 public class AccountsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
 	}
-
 }
